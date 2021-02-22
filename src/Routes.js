de7ef88fce4d4ced37bc9cdddb017e931095c99c
Roma-JS9 <rromapak2000@gmail.com>
2021-02-22 16:50:41 +0600

    asdf

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "./pages/Auth/Login/Login";
-import Register from "./pages/Auth/Register/Register";
-=======
 
 import Auth from "./pages/Auth/Auth";
->>>>>>> f449b7e8410b1b4e588064209104df49231ac71d
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
 import ProductsAdd from "./pages/Admin/ProductsAdd/ProductsAdd";
@@ -16,16 +11,6 @@ import ProductList from "./pages/Admin/ProductList/ProductList";
 
 const Routes = () => {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/" component={Login} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
-    </BrowserRouter>
-=======
     <ProductsContextProvider>
       <BrowserRouter>
         <Switch>
@@ -40,7 +25,6 @@ const Routes = () => {
         </Switch>
       </BrowserRouter>
     </ProductsContextProvider>
->>>>>>> f449b7e8410b1b4e588064209104df49231ac71d
   );
 };
 
