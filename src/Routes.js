6696842f6a79ca58c0a14f90517e124fdefce18c
Нурхан Жолдошев <zholdoshevnurhan@gmail.com>
2021-02-22 16:40:04 +0300

    asdf

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Auth from "./pages/Auth/Auth";
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Register/Register";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
 
@@ -8,7 +9,8 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Auth} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/cart" component={Cart} />
         <Route exact path="/home" component={Home} />
       </Switch>
