eec5ad37a6f10265506d062d7f7492ecf5839f5d
shaimbetov1@gmail.com <shaimbetov1@gmail.com>
2021-02-22 17:09:34 +0600

    asdf

--- src/Routes.js
+++ src/Routes.js
@@ -1,36 +1,23 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "./pages/Auth/Login/Login";
-import Register from "./pages/Auth/Register/Register";
-=======
 
-import Auth from "./pages/Auth/Auth";
->>>>>>> f449b7e8410b1b4e588064209104df49231ac71d
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
 import ProductsAdd from "./pages/Admin/ProductsAdd/ProductsAdd";
 import ProductsContextProvider from "./contexts/ProductsContext/ProductsContext";
 import List from "./pages/Products/List";
 import ProductList from "./pages/Admin/ProductList/ProductList";
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Register/Register";
 
 const Routes = () => {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/" component={Login} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
-    </BrowserRouter>
-=======
     <ProductsContextProvider>
       <BrowserRouter>
         <Switch>
 
-          <Route exact path="/" component={Auth} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
           <Route exact path="/cart" component={Cart} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/list" component={List} />
@@ -40,7 +27,6 @@ const Routes = () => {
         </Switch>
       </BrowserRouter>
     </ProductsContextProvider>
->>>>>>> f449b7e8410b1b4e588064209104df49231ac71d
   );
 };
 
