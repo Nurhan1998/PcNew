79a4bb364f716fbed127f5bdc4c50169f05321c4
Temir <https://github.com/Temir2700.git>
2021-02-22 16:53:40 +0600

    asdf

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Cart from "./pages/Cart/Cart";
+import FormPage from "./pages/FormPage/FormPage";
 import Home from "./pages/Home/Home";
 
 const Routes = () => {
-  return (
+  return(
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Auth} />
         <Route exact path="/cart" component={Cart} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/formpage" component={FormPage} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/FormPage/FormPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormPage = () => {
+    return (
+        <container>
+            <h1>Form</h1>
+        </container>
+    );
+};
+
+export default FormPage;
